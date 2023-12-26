bd3d3cdfd8e155d00332f5affd6c8fa6a253bf71
Amelia Nelson <153486779+only-agape@users.noreply.github.com>
2023-12-27 05:11:36 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// Promise and Async/Await with Fetch in JavaScript
+async function fetchData(url) {
+    // ... (asynchronous operation)
+}
+
+try {
+    const result = await fetchData('https://api.example.com/data');
+    // ... (handle result)
+} catch (error) {
+    // ... (handle errors)
+}
